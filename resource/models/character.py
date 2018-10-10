cd7728a818d36dbff26d3b94cf247b4b3517fc73
DeluxeYang <421826500@qq.com>
2018-10-10 14:20:25 +0800

    2018-10-10

--- resource/models/character.py
+++ resource/models/character.py
@@ -16,6 +16,10 @@ class Faction(models.Model):
     name = models.CharField(max_length=30)
     gender = models.IntegerField(choices=((-1, "female"), (1, "male"), (0, "either")))
     name_cn = models.CharField(max_length=30)
+    master = models.CharField(max_length=30, null=True, blank=True)
+    nick_name = models.CharField(max_length=30, null=True, blank=True)
+    describe = models.CharField(max_length=200, null=True, blank=True)
+    feature = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         if self.gender == -1:
@@ -25,6 +29,18 @@ class Faction(models.Model):
         else:
             gender = ""
         return self.race.name_cn + ": " + self.name_cn + gender
+
+
+class Skill(models.Model):
+    faction = models.ForeignKey(Faction, related_name='Skill', on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, unique=True)
+    name_cn = models.CharField(max_length=30)
+    level = models.IntegerField(default=1)
+    nick_name = models.CharField(max_length=30)
+    describe = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name_cn
 
 
 class Character(models.Model):
