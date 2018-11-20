c496665d1460509e6dea74629ffb959ececdea57
DeluxeYang <421826500@qq.com>
2018-11-20 15:40:55 +0800

    game server thread

--- manage.py
+++ manage.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 import os
 import sys
+from threading import Thread
+
+import django
+django.setup()
+
+from game.game_server import XY2GameServer
+from xy2onlineServer.settings import Network_Port
+
+
+def game_server_thread():
+    game_server = XY2GameServer(local_address=("localhost", int(Network_Port)))
+    game_server.launch()
 
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xy2onlineServer.settings')
     try:
         from django.core.management import execute_from_command_line
+
+        server = Thread(target=game_server_thread)
+        server.daemon = True
+        server.start()
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
