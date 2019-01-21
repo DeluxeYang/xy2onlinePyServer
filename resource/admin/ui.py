a94990241001a3f16d10b037e57d1802c04a26b9
DeluxeYang <421826500@qq.com>
2019-01-21 13:34:19 +0800

    display ui's hash code

--- resource/admin/ui.py
+++ resource/admin/ui.py
@@ -5,12 +5,15 @@ from xy2onlineServer.settings import STATIC_URL
 
 
 class UIAdmin(admin.ModelAdmin):
-    list_display = ("__str__", 'image_data')
-    readonly_fields = ('image_data',)
+    list_display = ("__str__","was_hash", "image_data")
+    readonly_fields = ("image_data", "was_hash")
     raw_id_fields = ("was", )
 
     def image_data(self, obj):
         return mark_safe(u'<img src="%s%s"/>' % (STATIC_URL, obj.was.image.url))
+
+    def was_hash(self, obj):
+        return obj.was.hash
 
     image_data.short_description = u'图片'
 
