63ef95161a30024c48a47481018de0a4b32e50fd
DeluxeYang <421826500@qq.com>
2019-01-24 13:05:33 +0800

    2019-1-24

--- base/models/account.py
+++ base/models/account.py
@@ -1,10 +1,18 @@
 from django.db import models
+import datetime
 
 
 # Create your models here.
 class Account(models.Model):
-    name = models.CharField(max_length=200)
+    account = models.CharField(max_length=200)
     character_num = models.IntegerField(default=0)
+    create_time = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.account
+
+    def save(self, *args, **kwargs):
+        if not self.create_time:
+            self.create_time = datetime.datetime.now()
+        super().save(*args, **kwargs)
+
