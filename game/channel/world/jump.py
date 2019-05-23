735edd0401d0cf36bc4323621104410d651fd592
DeluxeYang <421826500@qq.com>
2019-05-23 15:44:45 +0800

    2019-5-23

--- game/channel/world/jump.py
+++ game/channel/world/jump.py
@@ -19,3 +19,27 @@ def network_jump(self, data):
     }
     print(send_data)
     self.transmit(send_data)
+    send_data = {
+        'action': "remove_role",
+        'map_version': data["map_version"],
+        'map_id': data["map_id"],
+        'role_id': data["role_id"],
+        'role_name': data["role_name"]
+    }
+    self._server.broadcast(send_data, data['account'])
+    send_data = {
+        'action': "add_role",
+        'map_version': data["target_map_version"],
+        'map_id': data["target_map_id"],
+        'x': role_model.x,
+        'y': role_model.y,
+        'role_id': role_model.id,
+        'role_name': role_model.name,
+        'level': role_model.level,
+        'reborn': role_model.reborn,
+        'race': role_model.character.race.name,
+        'version': role_model.character.version_choices[role_model.character.version][1],
+        'character': role_model.character.name,
+        'gender': role_model.character.gender
+    }
+    self._server.broadcast(send_data, data['account'])
